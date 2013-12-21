3c477caec9c66175c41b60a5e3d99ab03e9342b6
Michael Stenta <mike@mstenta.net>
2013-12-21 16:31:21 -0500

    Modify to work with unlimited DS18B20 sensors.

--- measure.py
+++ measure.py
@@ -8,27 +8,32 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+device_folders = glob.glob(base_dir + '28*')
+device_files = []
+i = 0
+for folder in device_folders:
+    device_files.append(device_folders[i] + '/w1_slave')
+    i += 1
 
-def read_temp_raw():
-    f = open(device_file, 'r')
+def read_temp_raw(file):
+    f = open(file, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+    for file in device_files:
+        lines = read_temp_raw(file)
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw(file)
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            print file, ':', temp_c, 'C,', temp_f, 'F'
 
 while True:
-    print(read_temp())
+    read_temp()
     time.sleep(1)
